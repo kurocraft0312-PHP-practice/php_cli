0715162cfb6fe6a09103267c31f4b0ca5139f080
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-09 08:50:00 +0900

    add:人間クラスの作成

---
+++ classes/Human.php
@@ -0,0 +1,23 @@
+<?php
+    // 人間クラス
+    class Human
+    {
+        const MAX_HITPOINT = 100;
+        public $name;
+        public $hitPoint = 100; //現在のHP
+        public $attackPoint = 20; // 攻撃力
+        public function doAttack($enemy)
+        {
+            echo "『" . $this->name . "』の攻撃！\n";
+            echo "【" . $enemy->name . "】に" . $this->attackPoint . "のダメージ！\n";
+            $enemy->tookDamage($this->attackPoint);
+        }
+        public function tookDamage($damage)
+        {
+            $this->hitPoint -= $damage; // HPからダメージを計算して残りのHPを表示する
+            if ($this->hitPoint < 0) { // HPがマイナスにならないよう下限を設定する
+                $this->hitPoint = 0;
+            }
+        }
+    }
+?>
