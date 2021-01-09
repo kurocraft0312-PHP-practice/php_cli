7e89b63130de202df0857ae5b649d0988a43c71f
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-09 14:32:24 +0900

    add:1章のクラス作成完了

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
-   echo "Hello";
+   require_once('classes/Human.php');
+   require_once('classes/Enemy.php');
+
+   $tiida = new Human();
+   $goblin = new Enemy();
+
+   $tiida->name = "ティーダ";
+   $goblin->name = "ゴブリン";
+
+   $turn = 1; // ターン
+
+   while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {// どちらかのHPが0になるまでループ
+      echo "*** $turn ターン目 ***\n\n";
+
+      echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+      echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
+      echo "\n";
+
+      $tiida->doAttack($goblin);
+      echo "\n";
+      $goblin->doAttack($tiida);
+      echo "\n";
+
+      $turn++;
+}
+
+   echo "★★★ 戦闘終了 ★★★\n\n";
+   echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+   echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
+
 ?>
