96c443c3c2481cd2dbe199f7760ae9681638e085
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-16 21:44:59 +0900

    fix:不要なコメントを削除

--- classes/Enemy.php
+++ classes/Enemy.php
@@ -2,54 +2,11 @@
     class Enemy extends Lives
     {
         const MAX_HITPOINT = 50;
-        // private $name;
-        // private $hitPoint = 50;
-        // private $attackPoint = 10;
-
         public function __construct($name, $attackPoint)
         {
-            // $this->name = $name;
-            // $this->attackPoint = $attackPoint;
             $hitPoint = 50;
             $intelligence = 0;
             parent::__construct($name,$hitPoint,$attackPoint,$intelligence);
         }
-        
-        // public function getName()
-        // {
-        //     return $this->name;
-        // }
-
-        // public function getHitPoint()
-        // {
-        //     return $this->hitPoint;
-        // }
-
-        // public function getAttackPoint()
-        // {
-        //     return $this->attackPoint;
-        // }
-
-        // public function doAttack($humans)
-        // {
-        //     if ($this->hitPoint <= 0) {
-        //         return false;
-        //     }
-
-        //     $humanIndex = rand(0, count($humans) -1);
-        //     $human = $humans[$humanIndex];
-
-
-        //     echo "『" . $this->getName() . "』の攻撃！\n";
-        //     echo "『" . $human->getName() . "』に" . $this->attackPoint . "のダメージ！\n";
-        //     $human->tookDamage($this->attackPoint);
-        // }
-        // public function tookDamage($damage)
-        // {
-        //     $this->hitPoint -= $damage;
-        //     if ($this->hitPoint < 0) { // HPの下限を0に設定する
-        //         $this->hitPoint = 0;
-        //     }
-        // }  
     }
 ?>
--- classes/Human.php
+++ classes/Human.php
@@ -2,15 +2,9 @@
     class Human extends Lives
     {
         const MAX_HITPOINT = 100;
-        // private $name;
-        // private $hitPoint = 100; //現在のHP
-        // private $attackPoint = 20; // 攻撃力
 
         public function __construct($name,$hitPoint = 100, $attackPoint = 20, $intelligence = 0)
         {
-            // $this->name = $name;
-            // $this->hitPoint = $hitPoint;
-            // $this->attackPoint = $attackPoint;
             parent::__construct($name, $hitPoint, $attackPoint, $intelligence);
         }
     }
