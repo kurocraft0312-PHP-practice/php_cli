0cc7cd466d581ecd0760ab343026913d5da4e4a7
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-16 23:47:29 +0900

    fix:キャラクター名を定数化

---
+++ classes/constants/EnemyName.php
@@ -0,0 +1,11 @@
+<?php
+/***********************
+エネミー名の定数を保持する
+***********************/
+    class EnemyName
+    {
+        const GOBLINS = 'ゴブリン';
+        const BOMB = 'ボム';
+        const MORBOL = 'モルボル';
+    }
+?>
